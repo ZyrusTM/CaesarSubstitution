0b6bc404d774845d623795985334257c812cc8ef
Erik Bommes <erik@hahn-berlin.net>
2022-11-17 20:26:27 +0100

    improved code quality

--- CaesarSubstitution2.0/CaesarSubstitution.cpp
+++ CaesarSubstitution2.0/CaesarSubstitution.cpp
@@ -39,10 +39,10 @@ void CaesarSubstitution::encrypt()
 		encAscii += 65; //After the operation, you have to add 65, to get back in Ascii's uppercase-alphabet range
 		c = (char)encAscii;
 	}
-	string str(m_textArr.begin(), m_textArr.end());
+	string encText(m_textArr.begin(), m_textArr.end());
 
 	cout << "Encrypted: " << m_inputText << endl;
-	cout << "To: " << str << endl;
+	cout << "To: " << encText << endl;
 	cout << "Using key: " << m_key << endl;
 }
 
@@ -51,20 +51,20 @@ void CaesarSubstitution::decrypt()
 	for (char& c : m_textArr) {
 		int ascii = (int)c;
 		ascii -= 65; 
-		int encAscii = (ascii - m_key) % 26;
-		if (encAscii < 0) {
-			encAscii += 26;
+		int decAscii = (ascii - m_key) % 26;
+		if (decAscii < 0) {
+			decAscii += 26;
 		}
 
-		encAscii += 65; 
-		c = (char)encAscii;
+		decAscii += 65; 
+		c = (char)decAscii;
 	}
-	string str;
+	string decText;
 
 	for (int i = 0;i < m_textlength;i++) {
-		str += m_textArr[i];
+		decText += m_textArr[i];
 	}
 	cout << "Decrypted: " << m_inputText << endl;
-	cout << "To: " << str << endl;
+	cout << "To: " << decText << endl;
 	cout << "Using key: " << m_key << endl;
 }
